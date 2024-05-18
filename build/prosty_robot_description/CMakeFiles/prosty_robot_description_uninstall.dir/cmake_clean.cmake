b9f25136c5b6ae6b4c514c7b42fcf4057c6a4544
Bartek <sedbar12@gmial.com>
2024-05-18 14:28:31 +0200

    123

---
+++ build/prosty_robot_description/CMakeFiles/prosty_robot_description_uninstall.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prosty_robot_description_uninstall"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/prosty_robot_description_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
